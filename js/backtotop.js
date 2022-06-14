cfd2b6e2ce1121647b2be705c7872afd86f43307
tri-nm2 <nguyenminhtri19031994@gmail.com>
2022-06-14 09:38:46 +0700

    feature: implement button back to top

---
+++ js/backtotop.js
@@ -0,0 +1,16 @@
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("backToTop").style.display = "block";
+  } else {
+    document.getElementById("backToTop").style.display = "none";
+  }
+}
+
+function toTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
