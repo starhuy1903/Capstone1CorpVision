23eb3de59c9ecbf6c49f5f34aa0561a699ab58f4
Huy Nguyen <huykst137@gmail.com>
2022-06-27 12:59:19 +0700

    add: feature for button dark mode and light mode

--- js/header.js
+++ js/header.js
@@ -1,32 +1,40 @@
-var btnLight = document.getElementById("btnLight");
-var btnDark = document.getElementById("btnDark");
-
-btnLight.onclick = function () {
-  if (
-    btnLight.classList.contains("display") === true &&
-    btnDark.classList.contains("display") === false
-  ) {
-    btnLight.classList.remove("display");
-    btnDark.classList.add("display");
-  }
-};
-
-btnDark.onclick = function () {
-  if (
-    btnDark.classList.contains("display") === true &&
-    btnLight.classList.contains("display") === false
-  ) {
-    btnDark.classList.remove("display");
-    btnLight.classList.add("display");
-  }
+// Grab elements
+const selectElement = (selector) => {
+  const element = document.querySelector(selector);
+  if (element) return element;
+  throw new Error(
+    `Something went wrong! Make sure that ${selector} exists/is typed correctly.`
+  );
 };
 
 $(window).on("scroll", function () {
   var scroll = $(window).scrollTop();
 
   if (scroll >= 80) {
-      $("#header").addClass("fixed");
+    $("#header").addClass("fixed");
   } else {
-      $("#header").removeClass("fixed");
+    $("#header").removeClass("fixed");
+  }
+});
+
+// Switch theme/add to local storage
+const body = document.body;
+const themeToggleBtn = selectElement("#theme-toggle-btn");
+const currentTheme = localStorage.getItem("currentTheme");
+
+// Check to see if there is a theme preference in local Storage, if so add the light theme to the body
+if (currentTheme) {
+  body.classList.add("light-theme");
+}
+
+themeToggleBtn.addEventListener("click", function () {
+  // Add light theme on click
+  body.classList.toggle("light-theme");
+
+  // If the body has the class of light theme then add it to local Storage, if not remove it
+  if (body.classList.contains("light-theme")) {
+    localStorage.setItem("currentTheme", "themeActive");
+  } else {
+    localStorage.removeItem("currentTheme");
   }
 });
